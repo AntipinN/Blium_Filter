b3b0de93f0cca5259f597208cfd324f7f5e3f0e6
AntipinN <antipinko.nik@yandex.ru>
2026-05-23 16:46:41 +0300

    Повенял файл gitignore

--- Blum_Filter/obj/Debug/net8.0/Blum_Filter.AssemblyInfo.cs
+++ Blum_Filter/obj/Debug/net8.0/Blum_Filter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blum_Filter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5086043dffb48ddaf6bfafdf334c771c05955f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blum_Filter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blum_Filter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
